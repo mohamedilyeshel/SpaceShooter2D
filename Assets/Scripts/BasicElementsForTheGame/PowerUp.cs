d2fd2d6fb4ce313e618246bb4dc202679f4e441c
DexterFF <mohamedhelalilyes@gmail.com>
2020-09-25 05:42:56 +0200

    Phase II : Feature 11 : Pickup Collect

--- Assets/Scripts/BasicElementsForTheGame/PowerUp.cs
+++ Assets/Scripts/BasicElementsForTheGame/PowerUp.cs
@@ -7,6 +7,7 @@ public class PowerUp : MonoBehaviour
     [Header("Power Up State")]
     private float _speed;
     private int i = -1;
+    private bool _movingToPlayer = false;
 
     public enum powerUpsTypes
     {
@@ -26,13 +27,39 @@ public class PowerUp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        _speed = Random.Range(3, 5);
+        _speed = Random.Range(3, 5);        
+    }
+
+    private void OnEnable()
+    {
+        _movingToPlayer = false;
+        transform.rotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
         Mouvements();
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            _movingToPlayer = true;
+        }
+
+        if (currentPowerUp == powerUpsTypes.health && GameManager.Instance.health == 3)
+        {
+            _movingToPlayer = false;
+        }
+
+        if (currentPowerUp == powerUpsTypes.ammoCollect && PoolingManager.Instance.bulletsAmmount == 15)
+        {
+            _movingToPlayer = false;
+        }
+
+        if (_movingToPlayer == true)
+        {
+            GoTowardsPlayer();
+        }
     }
 
     void Mouvements()
@@ -42,6 +69,13 @@ public class PowerUp : MonoBehaviour
             gameObject.SetActive(false);
         }
         transform.Translate(Vector3.down * Time.deltaTime * _speed);
+    }
+
+    public void GoTowardsPlayer()
+    {
+        Vector2 direction = GameManager.Instance.CalculateDirectionBetweenPlayerAndB(this.transform);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 90f;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
